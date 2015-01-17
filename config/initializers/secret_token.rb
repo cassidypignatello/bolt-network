991a98d51455ba4515336b3fe2626f91f1eaaec8
cassidypignatello <cassidy.pignatello@gmail.com>
2015-01-17 17:01:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoltNetwork::Application.config.secret_token = 'cf5ba02232c2dd886c62910e7442b7662c07621dd0427811b67a3d602bde197e46377da975ee9821860cbf9cee36b2e5be7b3f24c01155ab3e32dca11bc8f7fc'
